2d622ed4d58c8a6d970e872ee905d4529b15a5a2
Kaltham Ahashmi <s202371470@kfupm.edu.sa>
2026-03-30 13:16:01 +0300

    TODO 6

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -12,22 +12,25 @@ const app = express();
 app.use(cors());
 
 
-// TODO 4: Start the server on port 3000
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
 // TODO 5: Create the home route "/"
+
 app.get('/', (req, res) => {
   res.send('Hello from the back-end server');
 });
 
 
 // TODO 6: Create the "/about" route
-
+app.get('/about', (req, res) => {
+  res.send('This is the about route');
+});
 
 
 // TODO 7: Create the "/student" route
 
 
 
+// TODO 4: Start the server on port 3000
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
 
